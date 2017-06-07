0db12f6f4d392269f514998698ce447f0ca8a07a
Mohammad <mohammad.altahan@sharedop.com>
2017-06-07 11:37:20 +0200

    Initilize the project and add table to the pdf file

---
+++ PdfExport/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PdfExport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public static RenderTargetBitmap GetImage(UIElement view)
+        {
+            Size size = new Size(view.RenderSize.Width, view.RenderSize.Height);
+            if (size.IsEmpty)
+                return null;
+
+            RenderTargetBitmap result = new RenderTargetBitmap((int)size.Width, (int)size.Height, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual drawingvisual = new DrawingVisual();
+            using (DrawingContext context = drawingvisual.RenderOpen())
+            {
+                context.DrawRectangle(new VisualBrush(view), null, new Rect(0, 0, (int)size.Width, (int)size.Height));
+                context.Close();
+            }
+
+            result.Render(drawingvisual);
+            return result;
+        }
+
+        public static void createPdfFromImage(string imageFile, string pdfFile)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var document = new iTextSharp.text.Document(iTextSharp.text.PageSize.LETTER.Rotate(), 0, 0, 0, 0);
+                PdfWriter.GetInstance(document, new FileStream(pdfFile, FileMode.Create));
+                iTextSharp.text.pdf.PdfWriter.GetInstance(document, ms).SetFullCompression();
+                document.Open();
+
+                FileStream fs = new FileStream(imageFile, FileMode.Open);
+                var image = iTextSharp.text.Image.GetInstance(fs);
+                image.ScaleToFit(document.PageSize.Width, document.PageSize.Height);
+                document.Add(image);
+                document.Close();
+
+                //open pdf file
+                Process.Start("explorer.exe", pdfFile);
+            }
+        }
+    }
+}
