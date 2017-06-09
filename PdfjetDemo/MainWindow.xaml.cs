fb1aba83236f6ebff4957b838e9607eaa865bce3
Mohammad <mohammad.altahan@sharedop.com>
2017-06-09 12:38:00 +0200

    Revert "Adding Tables to the pdf file"
    This reverts commit 9e11d9c5fa56e23d82dfba82eaf64c4f8127a91d.

--- PdfjetDemo/MainWindow.xaml.cs
+++ PdfjetDemo/MainWindow.xaml.cs
@@ -23,8 +23,7 @@ namespace PdfjetDemo
 
         public void DrawComponent()
         {
-#region   initilize the pdf file
-
+            //initilize the pdf file
             FileStream fos = new FileStream("PdfJet.pdf", FileMode.Create);
 
             BufferedStream bos = new BufferedStream(fos);
@@ -41,10 +40,7 @@ namespace PdfjetDemo
 
             Labels_Font.SetSize(10f);
 
-            #endregion
-
-#region Draw the Logo
-
+            // Draw the Logo
             String fileName = @"C:\Users\Fatima\Documents\PdfExport\PdfjetDemo\images\pink_elephant.png";
 
             FileStream fis1 = new FileStream(fileName, FileMode.Open, FileAccess.Read);
@@ -57,9 +53,7 @@ namespace PdfjetDemo
 
             image2.DrawOn(page2);
 
-            #endregion
-
-#region Draw the title
+            //Draw the title
 
             TextColumn column = new TextColumn(0);
 
@@ -80,9 +74,8 @@ namespace PdfjetDemo
             column.AddParagraph(p1);
 
             column.DrawOn(page2);
-            #endregion
 
-#region Draw the Form
+            //Draw the Form
 
             // Set the Colomn for the labels
 
@@ -98,7 +91,6 @@ namespace PdfjetDemo
 
             Labels_Colomn.SetSpaceBetweenParagraphs(20);
 
-
             //Set The Text for the labels
 
             Paragraph First_Name = new Paragraph();
@@ -109,62 +101,19 @@ namespace PdfjetDemo
 
             Last_Name.Add(new TextLine(Labels_Font, "Last Name:"));
 
-            Paragraph Gender = new Paragraph();
-
-            Gender.Add(new TextLine(Labels_Font, "Gender:"));
-
-            Paragraph Favourite_sport = new Paragraph();
-
-            Favourite_sport.Add(new TextLine(Labels_Font, "sport:"));
+            // Create the textboxes for the labels
 
             TextBox First_Name_Textbox = new TextBox(Labels_Font);
 
             First_Name_Textbox.SetText("Mohammad");
 
-            First_Name_Textbox.SetPosition(80.0f, 80.0f);
-
+            First_Name_Textbox.SetPosition(80.0f,80.0f);
+           
             TextBox Last_Name_Textbox = new TextBox(Labels_Font);
 
             Last_Name_Textbox.SetText("Altahan");
 
             Last_Name_Textbox.SetPosition(80.0f, 110.0f);
-
-
-            //add Radio buttons to the form
-
-            RadioButton Male_Radiobutton = new RadioButton(Labels_Font, "Male");
-
-            Male_Radiobutton.SetPosition(80.0f, 150.0f);
-
-            Male_Radiobutton.Select(true);
-
-            RadioButton Female_Radiobutton = new RadioButton(Labels_Font, "Female");
-
-            Female_Radiobutton.SetPosition(130.0f, 150.0f);
-
-            //add Radio buttons to the form
-
-            CheckBox Football_checkbox = new CheckBox(Labels_Font, "Football");
-
-            Football_checkbox.SetPosition(80.0f, 180.0f);
-
-            Football_checkbox.Check(Mark.CHECK);
-
-            CheckBox swimming_checkbox = new CheckBox(Labels_Font, "Swimming");
-
-            swimming_checkbox.SetPosition(150.0f, 180.0f);
-
-            swimming_checkbox.Check(Mark.CHECK);
-
-            CheckBox joggen_checkbox = new CheckBox(Labels_Font, "joggen");
-
-            joggen_checkbox.SetPosition(220.0f, 180.0f);
-
-            #endregion
-
-          
-
-            #region  add component to the page
 
             //add the labels to the colomn
 
@@ -172,140 +121,17 @@ namespace PdfjetDemo
 
             Labels_Colomn.AddParagraph(Last_Name);
 
-            Labels_Colomn.AddParagraph(Gender);
-
-            Labels_Colomn.AddParagraph(Favourite_sport);
-           
-
-            //add the Textboxes to the page
+            //add the the components to the page
 
             First_Name_Textbox.DrawOn(page2);
 
             Last_Name_Textbox.DrawOn(page2);
 
-
-            //add the column to the page
-
             Labels_Colomn.DrawOn(page2);
 
-            //add the Radio buttons to the page
-
-            Male_Radiobutton.DrawOn(page2);
-
-            Female_Radiobutton.DrawOn(page2);
-
-            //add the Checkboxes to the page
-
-            Football_checkbox.DrawOn(page2);
-
-            swimming_checkbox.DrawOn(page2);
-
-            joggen_checkbox.DrawOn(page2);
-            #endregion
-
-            #region  Add a table to the file
-
-            TextLine Table_text = new TextLine(Title_Font, "Some data about world communications");
-
-            Table_text.SetPosition(20.0f, 320.0f);
-
-            Table table = new Table();
-
-            Font Header_Font = new Font(pdf, CoreFont.HELVETICA_BOLD);
-
-            Header_Font.SetSize(7.0f);
-
-            Font Cell_Font = new Font(pdf, CoreFont.HELVETICA);
-            Cell_Font.SetSize(7.0f);
-
-            List<List<Cell>> tableData = GetData("WorldData.txt", "|", Table.DATA_HAS_2_HEADER_ROWS, Header_Font, Cell_Font);
-
-            table.SetData(tableData, Table.DATA_HAS_2_HEADER_ROWS);
-
-            table.SetPosition(20.0f, 240.0f);
-
-            table.SetTextColorInRow(6, Color.blue);
-
-            table.SetTextColorInRow(39, Color.red);
-
-            table.RemoveLineBetweenRows(0, 1);
-
-            table.AutoAdjustColumnWidths();
-
-            table.SetColumnWidth(0, 120);
-
-            table.RightAlignNumbers();
-
-            int numOfPages = table.GetNumberOfPages(page2);
-
-            Table_text.DrawOn(page2);
-
-
-            while (true)
-            {
-                table.DrawOn(page2);
-                // TO DO: Draw "Page 1 of N" here
-                if (!table.HasMoreData())
-                {
-                    // Allow the table to be drawn again later:
-                    table.ResetRenderedPagesCount();
-                    break;
-                }
-                page2 = new Page(pdf, Letter.PORTRAIT);
-            }
-
-            #endregion
-
-            #region Write to the pdf file
+            //Write to the pdf file
             pdf.Flush();
             bos.Close();
-#endregion
-        }
-
-
-        public List<List<Cell>> GetData(String fileName,String delimiter,int numOfHeaderRows,Font f1,Font f2)
-        {
-
-            List<List<Cell>> tableData = new List<List<Cell>>();
-
-            int currentRow = 0;
-            StreamReader reader = new StreamReader(fileName);
-            String line = null;
-            while ((line = reader.ReadLine()) != null)
-            {
-                List<Cell> row = new List<Cell>();
-                String[] cols = null;
-                if (delimiter.Equals("|"))
-                {
-                    cols = line.Split(new Char[] { '|' });
-                }
-                else if (delimiter.Equals("\t"))
-                {
-                    cols = line.Split(new Char[] { '\t' });
-                }
-                else
-                {
-                    throw new Exception(
-                            "Only pipes and tabs can be used as delimiters");
-                }
-                for (int i = 0; i < cols.Length; i++)
-                {
-                    String text = cols[i].Trim();
-                    if (currentRow < numOfHeaderRows)
-                    {
-                        row.Add(new Cell(f1, text));
-                    }
-                    else
-                    {
-                        row.Add(new Cell(f2, text));
-                    }
-                }
-                tableData.Add(row);
-                currentRow++;
-            }
-            reader.Close();
-
-            return tableData;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
